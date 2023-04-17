c1828563931ac22abeb8a5eddadff825e7f6c015
AslanPliev <aslanpliev22@gmail.com>
2023-04-17 16:17:18 +0300

    added empty and size

--- list/src/list.cc
+++ list/src/list.cc
@@ -1,5 +1,15 @@
 #include "list.h"
 
+
+
+/*typedef struct list_t list_t;
+typedef struct list_node_t list_node_t;
+
+typedef const char* keyT;
+typedef int valueT;*/
+
+
+
 struct list_node_t {
 	keyT key;
 	valueT value;
@@ -13,3 +23,62 @@ struct list_t
 	list_node_t *back;
 	int size;
 };
+
+list_t *list_create()
+{
+
+}
+
+void list_free(list_t *list)
+{
+
+}
+
+void list_push_back(list_t *list, keyT key, valueT value)
+{
+
+}
+void list_push_front(list_t *list, keyT key, valueT value)
+{
+
+}
+void list_pop_back(list_t *list)
+{
+
+}
+void list_pop_front(list_t *list)
+{
+
+}
+list_node_t *list_back(list_t *list)
+{
+
+}
+list_node_t *list_front(list_t *list)
+{
+
+}
+void list_move_upfront(list_t *list, list_node_t *node)   ///////
+{
+
+}
+int list_size(list_t *list)
+{
+	return list->size;
+}
+int list_empty(list_t *list)
+{
+	return list->size == 0;
+}
+keyT node_key(list_node_t *node)
+{
+
+}
+valueT node_value(list_node_t *node)
+{
+
+}
+void list_dump(list_t *list)
+{
+
+}
