3d63f3a0210241722cd3ca3f639c2cd9106684b1
cluzze <dm.clozze@gmail.com>
2023-04-19 16:55:01 +0300

    changed build system

--- LRUcache/src/LRUcache.cc
+++ LRUcache/src/LRUcache.cc
@@ -3,11 +3,11 @@
 #include "hash_table.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 struct lru_cache_t
 {
 	htab_t *htab;
-	list_t *cache;
 	int size;
 };
 
@@ -15,38 +15,39 @@ lru_cache_t *lru_create(int size)
 {
 	lru_cache_t *lru = (lru_cache_t*)calloc(1, sizeof(lru_cache_t));
 	lru->size = size;
-	lru->htab = htab_create(0, NULL);
-	lru->cache = list_create();
+	lru->htab = htab_create(size);
 	return lru;
 }
 
 void lru_free(lru_cache_t *lru)
 {
 	htab_free(lru->htab);
-	list_free(lru->cache);
 	free(lru);
 }
 
 int lru_lookup_update(lru_cache_t *lru, keyT key)
 {
 	list_node_t *find, *back;
+	list_t *cache;
 	keyT erase_key;
 
+	cache = htab_list(lru->htab);
 	find = htab_find(lru->htab, key);
+
+/*	list_dump(cache);
+	htab_dump(lru->htab);*/
 
 	if (!find)										// key not present in cache
 	{
-		if (list_size(lru->cache) == lru->size)		//cache is full
+		if (list_size(cache) == lru->size)			//cache is full
 		{
-			back = list_back(lru->cache);
+			back = list_back(cache);
 			erase_key = node_key(back);
 			htab_erase(lru->htab, erase_key);
-			list_pop_back(lru->cache);			
 		}
-		list_push_front(lru->cache, key, 0);
-		htab_insert(lru->htab, key);
+		htab_insert(lru->htab, key, 1);
 		return 0;									//cache miss
 	}
-	list_move_upfront(lru->cache, find);
+	list_move_upfront(cache, find);
 	return 1;										//cache hit
 }
